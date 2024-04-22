771b0cc45b6944038c54b1c95fe86c8046b054f9
Ismael Varela <info@varelait.com>
2024-04-22 16:37:29 -0400

    file service, read file line parser method

--- src/main/java/FileService/ReadFile.java
+++ src/main/java/FileService/ReadFile.java
@@ -4,20 +4,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static java.lang.Integer.parseInt;
 
 public class ReadFile {
 
+    public static final java.sql.Date today = new java.sql.Date(new java.util.Date().getTime());
+    public static final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     private static final Logger logger = LogManager.getLogger("regular");
 
-    public static boolean read(String file){
+    public static boolean reader(String file){
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
-            String st;
-            while ((st = br.readLine()) != null)
-                System.out.println(st);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String validLine = parseLine(line);
+                if (validLine != null)
+                    System.out.println(validLine);
+            }
 
             return true;
         } catch (Exception e) {
@@ -25,5 +35,33 @@ public class ReadFile {
             return false;
         }
 
+    }
+
+    public static String parseLine(String line){
+        if (line != null){
+            String[] fields = line.split("<|>|--|,");
+
+            if (fields.length != 4)
+                return null;
+
+            try {
+                Date bd = new Date(formatter.parse(fields[2]).getTime());
+                if (today.getYear() - bd.getYear() < 18)
+                    return null;
+
+                int group = parseInt(fields[3]);
+                if (group < 1)
+                    return  null;
+
+            }catch (Exception e){
+                logger.trace("While parsing file lines, invalid format.");
+                return null;
+            }
+
+            return "(" + fields[0] + "," + fields[1] + "," + fields[2] + "," + fields[3] + ")";
+            //if (fields[2].cont)
+        }
+
+        return null;
     }
 }
--- src/test/java/FileServiceTest/ReaderTest.java
+++ src/test/java/FileServiceTest/ReaderTest.java
@@ -11,9 +11,10 @@ public class ReaderTest {
 
     @Test
     public void readValidFile(){
-        String filePath = "/home/uriel/www/EmployeeDB/src/main/webapp/uploads/employeesGPT.csv";
+        //String filePath = "/home/uriel/www/EmployeeDB/src/main/webapp/uploads/employeesGPT.csv";
+        String filePath = "/home/uriel/www/EmployeeDB/src/main/webapp/uploads/employeesBadLines.csv";
 
-        boolean result = ReadFile.read(filePath);
+        boolean result = ReadFile.reader(filePath);
 
         assertTrue(result);
     }
