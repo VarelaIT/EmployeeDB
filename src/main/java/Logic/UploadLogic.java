084178a1f7f6064e49cb53b0211adb96cc050d90
Ismael Varela <info@varelait.com>
2024-04-23 12:09:54 -0400

    upload logic, start method completed

--- src/main/java/Logic/UploadLogic.java
+++ src/main/java/Logic/UploadLogic.java
@@ -1,8 +1,28 @@
 package Logic;
 
+import Persistence.IUploadRepository;
+import Persistence.UploadRepository;
+import UploadFileService.ManagerThread;
+
 public class UploadLogic implements IUploadLogic{
+
+    private final IUploadRepository uploadRepository;
+    private String test = null;
+
+    UploadLogic(){
+        this.uploadRepository = new UploadRepository();
+    }
+
+    public UploadLogic(String test){
+        this.test = test;
+        this.uploadRepository = new UploadRepository(test);
+    }
+
     @Override
     public Integer start(String fileName, String location) {
-        return null;
+        Integer processId = uploadRepository.create(fileName);
+        Thread fileProcess = new Thread (new ManagerThread(processId, location, test));
+        fileProcess.start();
+        return processId;
     }
 }
---
+++ src/test/java/LogicTests/UploadLogicTest.java
@@ -0,0 +1,42 @@
+package LogicTests;
+
+import Logic.IUploadLogic;
+import Logic.UploadLogic;
+import Persistence.IUploadStatus;
+import Persistence.TableSchemas;
+import Persistence.UploadRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UploadLogicTest {
+
+    private IUploadLogic uploadLogic = null;
+    private final String test = "test";
+    @BeforeEach
+    public void employeeStorageInitialization(){
+        TableSchemas.dropUploadsTable(test);
+        TableSchemas.createUploadsTable(test);
+        TableSchemas.dropFailedLinesTable(test);
+        TableSchemas.createFailedLinesTable(test);
+        TableSchemas.dropEmployeesTable(test);
+        TableSchemas.createEmployeesTable(test);
+        uploadLogic = new UploadLogic(test);
+    }
+
+    @Test
+    public void startUpload() throws InterruptedException {
+        String fileName = "logicFile";
+        String location = "/home/uriel/www/EmployeeDB/src/main/webapp/uploads/employeesGPT.csv";
+
+        Integer processId = uploadLogic.start(fileName, location);
+        Thread.sleep(2000);
+        IUploadStatus status = new UploadRepository(test).getStatus(processId);
+        System.out.println("Total lines: " + status.getTotal() + "\nSucceed: " + status.getCompleted() + "\nFailed: " + status.getFailed());
+
+        assertNotNull(status);
+        assertEquals(status.getTotal(), status.getCompleted() + status.getFailed());
+    }
+}
